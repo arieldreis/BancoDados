5b051252ae24dba5f04e0b59716e734dc3045d0e
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-04-21 16:08:14 -0300

    Desafios de Banco de  Dados
    Estou fazendo mais exercícios de banco de dados

--- desafios.sql
+++ desafios.sql
@@ -23,8 +23,18 @@ where sexo = 'F' and nacionalidade != 'Brasil' and nascimento between '1990-01-0
 select count(*) from dados
 where sexo = 'F' and altura > 1.90;
 /*DESAFIO 10: Uma lista com as profissões dos gafanhotos e seus respectivos quantitativos*/
+select profissao, count(*) from dados
+group by profissao
+order by profissao;
 /*DESAFIO 11: Quantos gafanhotos homens e mulheres nasceram após 01/JAN/2005? */
+select sexo, count(*) from dados
+where nascimento > '2005-01-01'
+group by sexo;
 /*DESAFIO 12: Uma lista com os gafanhotos que nasceram fora do Brasil mostrando o país de origem
 e o total de pessoas nascidas lá. Só nos interessam os países que tiverem mais de 3 gafanhotos com essa nacionalidade*/
+select nacionalidade, count(*) from dados
+where nacionalidade != "Brasil"
+group by nacionalidade
+having count(nacionalidade) > 3;
 /*DESAFIO 13: Uma lista agrupada pela altura dos gafanhotos, mostrando quantas pessoas pesam acima de 100kg
 e que estão acima da média de altura de todos cadastrados*/
